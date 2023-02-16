a328f17e2c3af85e5fb50c2bf3205293a9d5990b
Charles Karlo F. Leguarda <110873671+chrlskrlo@users.noreply.github.com>
2023-02-16 11:52:27 +0800

    1st commit
    Trying search filter for the first time, added a search bar from material UI and  JSON data set from online. The Highlight of this is .map and .filter is being used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './input.css'
+import JSONDATA from './MOCK_DATA.json'
+import { useState } from 'react'
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='flex justify-center align-center'>
+      <div className='mt-2.5 text-center font-serif' >
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <TextField onChange={event => { 
+            setSearchTerm(event.target.value) }}
+            fullWidth label="Search..."
+            id="fullWidth" />
+          {JSONDATA.filter((val) => {
+            if (searchTerm == '') {
+              return val
+            } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val
+            }
+          }).map((val, key) => {
+            return (
+              <div key={key}>
+                <p className='m-2 text-xl'>{val.first_name}</p>
+
+              </div>
+            )
+          })}
+        </Box>
+      </div>
+
     </div>
   );
 }
